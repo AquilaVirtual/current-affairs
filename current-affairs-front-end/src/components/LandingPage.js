a1d650ee147429419eeb09e97e84282f8463e868
David Loveday <davemech@yahoo.com>
2018-10-22 16:04:17 -0700

    initial commit

---
+++ current-affairs-front-end/src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class LandingPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+         } 
+
+    }
+
+    render() {
+        return (
+            <div>LandingPage Here</div>
+        )
+    }
+
+}
+
+export default LandingPage;
