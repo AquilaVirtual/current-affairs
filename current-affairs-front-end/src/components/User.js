8a42642af36c7d051e7505d0febea7ee9cbee131
David Loveday <davemech@yahoo.com>
2018-10-23 23:29:56 -0700

    add user component

---
+++ current-affairs-front-end/src/components/User.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class User extends React.Component {
+    
+        constructor(props) {
+            super(props);
+            this.state = {
+                username: '',
+                newUsername: '',
+                email: '',
+                newEmail: '',
+                password: '',
+                newPassword: '',
+                verifyPassword: '',               
+                error: false,
+                errorMessage: ''
+            };
+        }        
+  
+        render() {
+            return (
+                <div>                
+                
+                  </div>
+                    )
+                }
+            
+            }
+    export default User; 
