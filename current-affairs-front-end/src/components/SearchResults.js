c6e12b904d23ad4f82673cd7fdabba2f2949090a
David Loveday <davemech@yahoo.com>
2018-10-27 22:38:14 -0700

    delete Accounts component and add SearchResults component

---
+++ current-affairs-front-end/src/components/SearchResults.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class SearchResults extends React.Component {
+    
+        constructor(props) {
+            super(props);
+            this.state = {
+               
+            };
+        }       
+  
+        render() {
+            return (
+                <div>                
+                SearchResults
+                  </div>
+                    )
+                }
+            
+            }
+    export default SearchResults; 
