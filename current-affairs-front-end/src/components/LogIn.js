25fb35642c405edbaaf16491736354feda835dff
David Loveday <davemech@yahoo.com>
2018-10-28 20:53:31 -0700

    initial deployment

--- current-affairs-front-end/src/components/LogIn.js
+++ current-affairs-front-end/src/components/LogIn.js
@@ -5,7 +5,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import SignUp from './SignUp';
+// import SignUp from './SignUp';
 
 import '../css/LogIn.css';
 class LogIn extends React.Component {
