2801d83ec556963085852473f7b39acd1166b2c6
David Loveday <davemech@yahoo.com>
2019-01-07 23:18:44 -0800

    add a placeholder image and format document

--- current-affairs-front-end/src/components/NewsCard.js
+++ current-affairs-front-end/src/components/NewsCard.js
@@ -1,23 +1,43 @@
 import React from "react";
 import moment from "moment";
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
-const NewsCard = (props) => {
-    return (
+const NewsCard = props => {
+  let url = props.article.urlToImage
+    ? props.article.urlToImage
+    : "https://mk0globalbankin3xg02.kinstacdn.com/wp-content/uploads/2018/09/business-news-3.jpg";
+  return (
     <div className="data">
       <Card>
         <CardBody>
-          <CardTitle style={{ fontSize:'30px', color: 'purple' }}>{props.article.title}</CardTitle>         
-          <CardText>{props.article.content}</CardText>   
+          <CardTitle style={{ fontSize: "30px", color: "purple" }}>
+            {props.article.title}
+          </CardTitle>
+          <CardText style={{ margin: "14px 0" }}>
+            {props.article.content}
+          </CardText>
           <CardText>
-            <small className="text-muted">{moment(props.article.publishedAt).calendar()}</small>
-          </CardText>      
-        <CardImg  width="50%" src={props.article.urlToImage} alt="Card image cap" />
+            <small className="text-muted" style={{ margin: "14px 0" }}>
+              {moment(props.article.publishedAt).calendar()}
+            </small>
+          </CardText>
+          <CardImg
+            style={{ margin: "14px 0", width: "500px", height: "300px" }}
+            src={url}
+            alt="Article image"
+          />
         </CardBody>
       </Card>
     </div>
   );
-}
+};
 
 export default NewsCard;
